27823d1551059a6fb6158afcb3204610a412c1ea
Yaretzy Charo <yaretzycharo@my.unt.edu>
2022-02-01 08:30:23 -0600

    completed the student entity table

--- leaf/src/main/java/com/example/leaf/model/student.java
+++ leaf/src/main/java/com/example/leaf/model/student.java
@@ -1,4 +1,107 @@
 package com.example.leaf.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "students")
 public class student {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private String workType;
+
+    @Column
+    private String email;
+
+    @Column
+    private String phone;
+
+    @Column
+    private Boolean isActive;
+
+    //here map to the plant_id so that have list of plants
+
+
+    //constructors
+
+    public student() {
+    }
+
+    public student(Long id, String firstName, String lastName, String workType, String email, String phone, Boolean isActive) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.workType = workType;
+        this.email = email;
+        this.phone = phone;
+        this.isActive = isActive;
+    }
+    //getters and setters
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getWorkType() {
+        return workType;
+    }
+
+    public void setWorkType(String workType) {
+        this.workType = workType;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Boolean getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(Boolean isActive) {
+        this.isActive = isActive;
+    }
 }
