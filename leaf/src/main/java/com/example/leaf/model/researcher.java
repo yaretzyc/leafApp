c6182b2948efbfcd134318e63bd28ba54cec6447
Yaretzy Charo <yaretzycharo@my.unt.edu>
2022-02-01 08:11:11 -0600

    added email to researcher and created table for section with getters and setters/constructors

--- leaf/src/main/java/com/example/leaf/model/researcher.java
+++ leaf/src/main/java/com/example/leaf/model/researcher.java
@@ -21,19 +21,23 @@ public class researcher {
     @Column
     private String phone;
 
+    @Column
+    private String email;
+
     //map to section to get section id here
-    
+
 
     //constructors
 
     public researcher() {
     }
 
-    public researcher(Long id, String firstName, String lastName, String phone) {
+    public researcher(Long id, String firstName, String lastName, String phone, String email) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.phone = phone;
+        this.email = email;
     }
 
 
@@ -70,5 +74,13 @@ public class researcher {
 
     public void setPhone(String phone) {
         this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
--- leaf/src/main/java/com/example/leaf/model/section.java
+++ leaf/src/main/java/com/example/leaf/model/section.java
@@ -1,4 +1,60 @@
 package com.example.leaf.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "sections")
 public class section {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    private String type;
+
+    private String location;
+
+
+    //map to the researcher to have the id here
+    // one researcher is in charge of one or many sections
+
+
+    //constructors
+    public section() {
+    }
+
+    public section(Long id, String type, String location) {
+        Id = id;
+        this.type = type;
+        this.location = location;
+    }
+
+
+    //getters and setters
+
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
