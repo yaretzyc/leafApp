b5d9a9a385122ff37da7afae6fbdb7a6d1deaa43
Yaretzy Charo <yaretzycharo@my.unt.edu>
2022-02-01 08:05:20 -0600

    uncommented jpa dependency to do orm mapping for the models. also created table researcher

--- leaf/src/main/java/com/example/leaf/model/researcher.java
+++ leaf/src/main/java/com/example/leaf/model/researcher.java
@@ -1,4 +1,74 @@
 package com.example.leaf.model;
 
+import javax.annotation.processing.Generated;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "researchers")
 public class researcher {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private String phone;
+
+    //map to section to get section id here
+    
+
+    //constructors
+
+    public researcher() {
+    }
+
+    public researcher(Long id, String firstName, String lastName, String phone) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+    }
+
+
+    //getters and setters
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
